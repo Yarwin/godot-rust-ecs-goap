2f73a081d9065b9e031d7fc792727ac69473ee23
Yarvin <yavinlecretin@gmail.com>
2022-09-15 14:56:30 +0200

    [001] - Minor code cleanup. [002] - Test init
    ====
    * Action for goap_planner module is now independent of individual user Action implementation – created trait Action that declares couple of getters and setters
    * "allowed actions" for goap module now require providing Vec of Actions with their associated cost
    * Naive/example actions implementations specific for GODOT-ECS-GOAP have their very own traits declared
    * GodotNode:godot_entity is now Ref<Node2D>, instead of being Ref<Node>
    * Fixed wrongly typed/configured ToVariant/FromVariant for hecs::Entity in GodotEntityId wrapper

---
+++ godot-ecs-goap-tests/src/lib.rs
@@ -0,0 +1,35 @@
+use gdnative::prelude::*;
+use gdnative_core::godot_itest;
+
+
+#[no_mangle]
+pub extern "C" fn run_tests(
+    _data: *mut gdnative::libc::c_void,
+    _args: *mut gdnative::sys::godot_array,
+) -> gdnative::sys::godot_variant {
+    let mut status = true;
+    status &= test_it_works();
+    status &= test_it_fails();
+    Variant::new(status).leak()
+}
+
+godot_itest! { test_it_works {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
+
+
+
+
+
+fn init(handle: InitHandle) {
+}
+
+fn terminate(_term_info: &gdnative::init::TerminateInfo) {
+    gdnative::tasks::terminate_runtime();
+}
+
+gdnative::init::godot_gdnative_init!();
+gdnative::init::godot_nativescript_init!(init);
+gdnative::init::godot_gdnative_terminate!(terminate);
---
+++ tests/test_ecs.gd
@@ -0,0 +1,17 @@
+extends GutTest
+
+
+func test_it_works():
+	var smth = true
+	assert_eq(smth, true)
+	
+
+func test_ecs():
+	var gdn = GDNative.new()
+	var status = false;
+	gdn.library = load("res://tests/test_gnd.gdnlib")
+	if gdn.initialize():
+		status = gdn.call_native("standard_varcall", "run_tests", [])
+		gdn.terminate()
+	assert_eq(status, true)
+
